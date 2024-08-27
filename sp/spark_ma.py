f381d2e8bdf00a3efc48dcd745bdc102e8fff44b
RaphaFang <fangsihyu0211@gmail.com>
2024-08-27 22:46:11 +0800

    test: left only 5_MA for testing without withWatermark

--- sp/spark_ma.py
+++ sp/spark_ma.py
@@ -3,8 +3,6 @@ from pyspark.sql.window import Window
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType, BooleanType, TimestampType
 from pyspark.sql.functions import from_json, col, window, sum as spark_sum, count as spark_count,avg, last, lit, to_timestamp, current_timestamp
 from pyspark.sql import functions as SF
-import six
-
 
 def main():
     schema = StructType([
@@ -38,9 +36,10 @@ def main():
     
 
     def calculate_sma(df, window_duration, column_name):
-        df_with_watermark = df.withWatermark("start", "15 seconds")
-
-        sma_df = df_with_watermark.groupBy(
+        # df_with_watermark = df.withWatermark("start", "10 seconds")
+        # sma_df = df_with_watermark.groupBy(
+        
+        sma_df = df.groupBy(
             SF.window(col("start"), f"{window_duration} seconds", "1 second"), col("symbol")
         ).agg(
             SF.sum(SF.when(col("size_per_sec") != 0, col("vwap_price_per_sec")).otherwise(0)).alias('sum_of_vwap'),
@@ -103,24 +102,15 @@ def main():
     def process_batch(df, epoch_id):
         try:
             df = df.selectExpr("CAST(value AS STRING) as json_data") \
-                .select(from_json(col("json_data"), schema).alias("data")) \
+                .select(SF.from_json(col("json_data"), schema).alias("data")) \
                 .select("data.*")
             sma_5 = calculate_sma(df, 5, "sma_5")
-            sma_15 = calculate_sma(df, 15, "sma_15")
-            sma_30 = calculate_sma(df, 30, "sma_30")
+            # sma_15 = calculate_sma(df, 15, "sma_15")
+            # sma_30 = calculate_sma(df, 30, "sma_30")
 
             send_to_kafka(sma_5)
-            send_to_kafka(sma_15)
-            send_to_kafka(sma_30)
-            
-            # sma_5.selectExpr(
-            #     "CAST(symbol AS STRING) AS key",
-            #     "to_json(struct(*)) AS value"
-            # ).write \
-            #     .format("kafka") \
-            #     .option("kafka.bootstrap.servers", "kafka:9092") \
-            #     .option("topic", "kafka_MA_data") \
-            #     .save()
+            # send_to_kafka(sma_15)
+            # send_to_kafka(sma_30)
             
         except Exception as e:
             print(f"Error processing batch {epoch_id}: {e}")
@@ -136,27 +126,3 @@ if __name__ == "__main__":
 
 
 # ---------------------------------------------------------------------------------------------------
-            # df = df.selectExpr("CAST(value AS STRING) as json_data") \
-            #     .select(from_json(col("json_data"), schema).alias("data")) \
-            #     .select("data.*")
-            
-            # sma_5 = calculate_sma(df, 5, "sma_5").withColumnRenamed("window.start", "start_5").withColumnRenamed("window.end", "end_5")
-            # sma_10 = calculate_sma(df, 10, "sma_10").withColumnRenamed("window.start", "start_10").withColumnRenamed("window.end", "end_10")
-            # sma_30 = calculate_sma(df, 30, "sma_30").withColumnRenamed("window.start", "start_30").withColumnRenamed("window.end", "end_30")
-
-            # final_df = sma_5.join(sma_10, ["symbol", "start_5", "end_5"]) \
-            #                 .join(sma_30, ["symbol", "start_5", "end_5"]) \
-            #                 .select(
-            #                     "symbol",
-            #                     "start_5", "end_5", "sma_5", "5_data_count",
-            #                     "start_10", "end_10", "sma_10", "10_data_count",
-            #                     "start_30", "end_30", "sma_30", "30_data_count"
-            #                 )
-            # final_df.selectExpr(
-            #     "CAST(symbol AS STRING) AS key",
-            #     "to_json(struct(*)) AS value"
-            # ).write \
-            #     .format("kafka") \
-            #     .option("kafka.bootstrap.servers", "kafka:9092") \
-            #     .option("topic", "kafka_MA_data") \
-            #     .save()

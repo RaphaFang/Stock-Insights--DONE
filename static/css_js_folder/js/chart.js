77cd35b54f90ac8bb51b22df8f9ba6bc6a3c6510
RaphaFang <fangsihyu0211@gmail.com>
2024-08-29 11:59:09 +0800

    test: new js formate

--- static/css_js_folder/js/chart.js
+++ static/css_js_folder/js/chart.js
@@ -1,178 +1,180 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const charts = {}; // Store chart instances by symbol
+  const charts = {}; // 存储每个symbol对应的图表实例
 
   const createChartSet = (symbol) => {
-    const chartSetContainer = document.createElement("div");
-    chartSetContainer.className = "chart-set";
+    if (!charts[symbol]) {
+      const chartSetContainer = document.createElement("div");
+      chartSetContainer.className = "chart-set";
 
-    const priceChartContainer = document.createElement("div");
-    priceChartContainer.className = "chart-container";
-    const priceCanvas = document.createElement("canvas");
-    priceCanvas.id = `priceChart_${symbol}`;
-    priceChartContainer.appendChild(priceCanvas);
+      const priceChartContainer = document.createElement("div");
+      priceChartContainer.className = "chart-container";
+      const priceCanvas = document.createElement("canvas");
+      priceCanvas.id = `priceChart_${symbol}`;
+      priceChartContainer.appendChild(priceCanvas);
 
-    const sizeChartContainer = document.createElement("div");
-    sizeChartContainer.className = "chart-container";
-    const sizeCanvas = document.createElement("canvas");
-    sizeCanvas.id = `sizeChart_${symbol}`;
-    sizeChartContainer.appendChild(sizeCanvas);
+      const sizeChartContainer = document.createElement("div");
+      sizeChartContainer.className = "chart-container";
+      const sizeCanvas = document.createElement("canvas");
+      sizeCanvas.id = `sizeChart_${symbol}`;
+      sizeChartContainer.appendChild(sizeCanvas);
 
-    chartSetContainer.appendChild(priceChartContainer);
-    chartSetContainer.appendChild(sizeChartContainer);
-    document.getElementById("chartSetsContainer").appendChild(chartSetContainer);
+      chartSetContainer.appendChild(priceChartContainer);
+      chartSetContainer.appendChild(sizeChartContainer);
+      document.getElementById("chartSetsContainer").appendChild(chartSetContainer);
 
-    const labels = [];
-    const vwapPriceData = [];
-    const sma5Data = [];
-    const sizePerSecData = [];
-    const pricePercentageChange = [];
-    const priceColors = [];
+      const labels = [];
+      const vwapPriceData = [];
+      const sma5Data = [];
+      const sizePerSecData = [];
+      const pricePercentageChange = [];
+      const priceColors = [];
 
-    const priceCtx = document.getElementById(`priceChart_${symbol}`).getContext("2d");
-    const priceChart = new Chart(priceCtx, {
-      type: "line",
-      data: {
-        labels: labels,
-        datasets: [
-          {
-            label: "VWAP Price per Second",
-            data: vwapPriceData,
-            borderColor: "rgba(255, 99, 132, 1)",
-            borderWidth: 2,
-            fill: false,
-            yAxisID: "y1",
-          },
-          {
-            label: "5 Period SMA",
-            data: sma5Data,
-            borderColor: "rgba(75, 192, 192, 1)",
-            borderWidth: 2,
-            fill: false,
-            yAxisID: "y1",
-          },
-          {
-            label: "Price Change (%)",
-            data: pricePercentageChange,
-            borderColor: function (context) {
-              const index = context.dataIndex;
-              return priceColors[index];
+      const priceCtx = document.getElementById(`priceChart_${symbol}`).getContext("2d");
+      const priceChart = new Chart(priceCtx, {
+        type: "line",
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              label: "VWAP Price per Second",
+              data: vwapPriceData,
+              borderColor: "rgba(255, 99, 132, 1)",
+              borderWidth: 2,
+              fill: false,
+              yAxisID: "y1",
             },
-            borderWidth: 2,
-            fill: false,
-            yAxisID: "y2",
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y1: {
-            type: "linear",
-            position: "left",
-            title: {
-              display: true,
-              text: "Price",
+            {
+              label: "5 Period SMA",
+              data: sma5Data,
+              borderColor: "rgba(75, 192, 192, 1)",
+              borderWidth: 2,
+              fill: false,
+              yAxisID: "y1",
             },
-          },
-          y2: {
-            type: "linear",
-            position: "right",
-            title: {
-              display: true,
-              text: "Percentage Change (%)",
-            },
-            ticks: {
-              min: -10,
-              max: 10,
-              callback: function (value) {
-                return value + "%";
+            {
+              label: "Price Change (%)",
+              data: pricePercentageChange,
+              borderColor: function (context) {
+                const index = context.dataIndex;
+                return priceColors[index];
               },
+              borderWidth: 2,
+              fill: false,
+              yAxisID: "y2",
             },
-          },
-          x: {
-            type: "time",
-            time: {
-              unit: "second",
-              displayFormats: {
-                second: "h:mm:ss a",
-              },
-            },
-            title: {
-              display: true,
-              text: "Time",
-            },
-          },
+          ],
         },
-        plugins: {
-          zoom: {
-            pan: {
-              enabled: true,
-              mode: "x",
+        options: {
+          scales: {
+            y1: {
+              type: "linear",
+              position: "left",
+              title: {
+                display: true,
+                text: "Price",
+              },
             },
+            y2: {
+              type: "linear",
+              position: "right",
+              title: {
+                display: true,
+                text: "Percentage Change (%)",
+              },
+              ticks: {
+                min: -10,
+                max: 10,
+                callback: function (value) {
+                  return value + "%";
+                },
+              },
+            },
+            x: {
+              type: "time",
+              time: {
+                unit: "second",
+                displayFormats: {
+                  second: "h:mm:ss a",
+                },
+              },
+              title: {
+                display: true,
+                text: "Time",
+              },
+            },
+          },
+          plugins: {
             zoom: {
-              enabled: true,
-              mode: "x",
-            },
-          },
-        },
-      },
-    });
-
-    const sizeCtx = document.getElementById(`sizeChart_${symbol}`).getContext("2d");
-    const sizeChart = new Chart(sizeCtx, {
-      type: "bar",
-      data: {
-        labels: labels,
-        datasets: [
-          {
-            label: "Size per Second",
-            data: sizePerSecData,
-            backgroundColor: "rgba(54, 162, 235, 0.5)",
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            type: "linear",
-            position: "left",
-            title: {
-              display: true,
-              text: "Size",
-            },
-          },
-          x: {
-            type: "time",
-            time: {
-              unit: "second",
-              displayFormats: {
-                second: "h:mm:ss a",
+              pan: {
+                enabled: true,
+                mode: "x",
+              },
+              zoom: {
+                enabled: true,
+                mode: "x",
               },
             },
-            title: {
-              display: true,
-              text: "Time",
-            },
           },
         },
-        plugins: {
-          zoom: {
-            pan: {
-              enabled: true,
-              mode: "x",
-            },
-            zoom: {
-              enabled: true,
-              mode: "x",
-            },
-          },
-        },
-      },
-    });
+      });
 
-    charts[symbol] = { priceChart, sizeChart, labels, vwapPriceData, sma5Data, sizePerSecData, pricePercentageChange, priceColors };
+      const sizeCtx = document.getElementById(`sizeChart_${symbol}`).getContext("2d");
+      const sizeChart = new Chart(sizeCtx, {
+        type: "bar",
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              label: "Size per Second",
+              data: sizePerSecData,
+              backgroundColor: "rgba(54, 162, 235, 0.5)",
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              type: "linear",
+              position: "left",
+              title: {
+                display: true,
+                text: "Size",
+              },
+            },
+            x: {
+              type: "time",
+              time: {
+                unit: "second",
+                displayFormats: {
+                  second: "h:mm:ss a",
+                },
+              },
+              title: {
+                display: true,
+                text: "Time",
+              },
+            },
+          },
+          plugins: {
+            zoom: {
+              pan: {
+                enabled: true,
+                mode: "x",
+              },
+              zoom: {
+                enabled: true,
+                mode: "x",
+              },
+            },
+          },
+        },
+      });
+
+      charts[symbol] = { priceChart, sizeChart, labels, vwapPriceData, sma5Data, sizePerSecData, pricePercentageChange, priceColors };
+    }
   };
 
-  // WebSocket connection
+  // WebSocket连接
   const ws = new WebSocket("wss://raphaelfang.com/stock/v1/ws/data");
 
   ws.onopen = function () {
@@ -183,9 +185,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const incomingData = JSON.parse(event.data);
     const symbol = incomingData.symbol;
 
-    if (!charts[symbol]) {
-      createChartSet(symbol);
-    }
+    createChartSet(symbol);
 
     const { priceChart, sizeChart, labels, vwapPriceData, sma5Data, sizePerSecData, pricePercentageChange, priceColors } = charts[symbol];
 

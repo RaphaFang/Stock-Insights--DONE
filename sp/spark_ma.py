496a52ccd0139a15cb717ac820f0a6ed97e9c2b7
RaphaFang <fangsihyu0211@gmail.com>
2024-09-11 15:43:37 +0800

    test: delete start()

--- sp/spark_ma.py
+++ sp/spark_ma.py
@@ -167,7 +167,7 @@ def main():
         .trigger(continuous='1 second')\
         .outputMode("update") \
         .option("checkpointLocation", "/app/tmp/spark_checkpoints/spark_ma") \
-        .start()
+        # .start()
         # .trigger(processingTime='20 seconds') \
     query.awaitTermination()
 
